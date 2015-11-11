8bfcb4a401d431fbbfffb76498c9a839fbddae9d
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2015-11-11 13:28:07 +0100

    using jekyll's built-in sass preprocessor.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,70 @@
-function js(filename){
-  return 'assets/js/src/' + filename + '.js';
-}
-
-function jsArray(filenames){
-  return filenames.split(',').map(function(filename){
-    return js(filename.trim());
-  });
-}
-
-module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    /*sass: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'src/scss',
-          src: ['*.scss'],
-          dest: ['build/css', '_site/build/css'],
-          ext: '.css'
-        }]
-      },
-      options: {
-        style: 'compressed'
-      }
-    },*/
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-        sourceMap: true
-      }
-    },
-    watch: {
-      /*css: {
-        files: 'src/scss/*.scss',
-        tasks: ['sass']
-      },*/
-      scripts: {
-        files: 'assets/js/src/**/*.js',
-        tasks: ['uglify'],
-        options: {
-          livereload: true,
-        }
-      },
-      site_css: {
-        files: '_site/assets/css/*.css',
-        options: {
-          livereload: true,
-        }
-      }
-    }
-  });
-
-  // Load the plugins:
-  grunt.loadNpmTasks('grunt-contrib-sass');   // $ grunt sass
-  grunt.loadNpmTasks('grunt-contrib-uglify'); // $ grunt uglify
-  grunt.loadNpmTasks('grunt-contrib-watch');  // $ grunt watch
-
-  // Default task:
-  grunt.registerTask('default', ['watch']);   // $ grunt
-
-};
+// var path = require('path');
+// var merge = require('merge');
+//
+// module.exports = function(grunt) {
+//   // Load all grunt tasks we have installed:
+//   require("matchdep").filterAll("grunt-*").forEach(grunt.loadNpmTasks);
+//
+//   grunt.initConfig({
+//     clean: ["tmp/assets/css"],
+//     cssmin: {
+//       // Minify CSS using cssmin. CSS @imports are inlined this way (whereas the Sass task only inlines imported .sass files)
+//       dist: {
+//         files: [{
+//           expand: true,
+//           cwd: 'tmp/assets/css',
+//           src: ['*.css'],
+//           dest: '_site/assets/css',
+//           ext: '.css'
+//         }]
+//       }
+//     },
+//     notify: {
+//       cssmin: {
+//         options:{
+//           title: "CSS Files built",
+//           message: "CSSMin task complete"
+//         }
+//       }
+//     },
+//     sass: {
+//       // Compile Sass files to a tmp directory where the minifier will pick it up from there.
+//       dist: {
+//         options: {
+//           style: 'nested',
+//           update: true
+//         },
+//         files: [{
+//           expand: true,
+//           cwd: 'assets/css',
+//           src: ['*.scss'],
+//           dest: 'tmp/assets/css',
+//           ext: '.css'
+//         }]
+//       }
+//     },
+//     watch: {
+//       css: {
+//         // Watch for any changes that happen within the src directory:
+//         files: ["assets/**/*.scss"],
+//         tasks: ["css"]
+//       },
+//       livereload: {
+//         // Here we watch the files the sass task will compile to
+//         // These files are sent to the live reload server after sass compiles to them
+//         options: {
+//           livereload: true
+//         },
+//         files: ['_site/assets/**/*'],
+//       }
+//     }
+//   });
+//
+//   // Default task watches for changes:
+//   grunt.registerTask("default", ["watch"]);
+//
+//   // Builds all SASS
+//   grunt.registerTask("build", ["sass", "cssmin", "clean"]);
+//
+//   grunt.registerTask("css", ["sass", "newer:cssmin", "notify:cssmin"]);
+// };
