b9ded59e4d8ca5ee4d29ab612dbabcf259ef010a
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-02-08 23:18:15 -0500

    added some media queries for the nav sections. added an unstyled toggle for the nav in mobile.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// Performs the request to get the plugins, then runs the display function
+// Performs the request to get plugins.
 load_plugins = function(list) {
   var xhr = new XMLHttpRequest()
   xhr.addEventListener('load', function () { display_plugins(list, JSON.parse(this.responseText)) })
@@ -6,7 +6,7 @@ load_plugins = function(list) {
   xhr.send()
 };
 
-// Displays a list of plugins
+// Displays a list of plugins.
 display_plugins = function(list, plugins) {
   for (var gem in plugins) {
     var item;
@@ -18,7 +18,24 @@ display_plugins = function(list, plugins) {
   }
 }
 
+// Load the actual plugins.
 var list = document.querySelector('.supported-plugins');
 if (list != null) {
   load_plugins(list);
 }
+
+// Toggle the nav on click on mobile.
+var nav_toggle = document.querySelector('.nav__toggle');
+var navs = document.querySelectorAll('.nav--mobile');
+
+nav_toggle.onclick = function() {
+  for (var i = 0; i < navs.length; i++) {
+    var nav = navs[i];
+
+    if (nav.style.display === "block") {
+      navs[i].style.display = "none";
+    } else {
+      navs[i].style.display = "block";
+    }
+  }
+};
