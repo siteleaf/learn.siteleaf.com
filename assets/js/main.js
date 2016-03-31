940234e4e0a7e336c33963d82c3e3fb4ec7b64a2
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-03-30 20:23:02 -0400

    use vanilla JS for HTTP request.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,24 @@
-display_plugins = function() {
-  var data = $.ajax({
-    type: "GET",
-    url: "https://api.siteleaf.com/v2/versions",
-    error: function() {
-      console.log("could not load");
-    },
-    success: function(data) {
-      var list = document.querySelector('.supported-plugins')
-
-      if (list != null) {
-        for (var gem in data) {
-          var item = document.createElement('li');
-          item.innerHTML = '<a href="https://rubygems.org/gems/' + gem + '">' + gem + '</a>' + ' (' + data[gem] + ')';
-
-          list.appendChild(item);
-        }
-      }
-    }
-  });
-
-
+// Performs the request to get the plugins, then runs the display function
+load_plugins = function(list) {
+  var xhr = new XMLHttpRequest()
+  xhr.addEventListener('load', function () { display_plugins(list, JSON.parse(this.responseText)) })
+  xhr.open('GET', 'https://api.siteleaf.com/v2/versions')
+  xhr.send()
 };
 
-$(document).ready(function () {
+// Displays a list of plugins
+display_plugins = function(list, plugins) {
+  for (var gem in plugins) {
+    var item;
 
-  display_plugins();
+    item = document.createElement('li');
+    item.innerHTML = '<a href="https://rubygems.org/gems/' + gem + '">' + gem + '</a>' + ' (' + plugins[gem] + ')';
 
-  // load_plugins();
+    list.appendChild(item);
+  }
+}
 
-});
+var list = document.querySelector('.supported-plugins');
+if (list != null) {
+  load_plugins(list);
+}
