da88e3f256df65a940602591aa559ed960546210
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-04-17 11:44:45 -0400

    Revert "collapse sidebar nav lists on mobile."
    This reverts commit 21f250b505b9f644ca1fb9873a5094c0570e0679.

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@ var toggle = document.querySelector('.nav__toggle');
 var toggle_button = toggle.firstElementChild;
 var sidebar = document.querySelector('.main__sidebar');
 
-toggle.onclick = function () {
+toggle.onclick = function() {
   sidebar.classList.toggle("main__sidebar--mobile-hidden")
 
   if (sidebar.classList.contains("main__sidebar--mobile-hidden")) {
@@ -38,17 +38,3 @@ toggle.onclick = function () {
     toggle_button.innerHTML = "&#10006;"
   }
 };
-
-$(function () {
-
-
-  var section_toggle = $('.nav__section-toggle');
-
-  section_toggle.on("click", function() {
-    var list = $(this).siblings('.nav__list')
-
-    list.toggleClass('nav__list--mobile-hidden');
-
-    console.log(list);
-  });
-});
