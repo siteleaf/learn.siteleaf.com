c1e9072de7e541490de24ee3834878d71f6d6ab0
Ethan Turkeltaub <ethan@oakmade.com>
2016-05-02 10:04:36 -0400

    Updated Back Up Your Site and 36 other files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// Performs the request to get the plugins, then runs the display function
+// Performs the request to get plugins.
 load_plugins = function(list) {
   var xhr = new XMLHttpRequest()
   xhr.addEventListener('load', function () { display_plugins(list, JSON.parse(this.responseText)) })
@@ -6,7 +6,7 @@ load_plugins = function(list) {
   xhr.send()
 };
 
-// Displays a list of plugins
+// Displays a list of plugins.
 display_plugins = function(list, plugins) {
   for (var gem in plugins) {
     var item;
@@ -18,7 +18,23 @@ display_plugins = function(list, plugins) {
   }
 }
 
+// Load the actual plugins.
 var list = document.querySelector('.supported-plugins');
 if (list != null) {
   load_plugins(list);
 }
+
+// Toggle the nav on click on mobile.
+var toggle = document.querySelector('.nav__toggle');
+var toggle_button = toggle.firstElementChild;
+var sidebar = document.querySelector('.main__sidebar');
+
+toggle.onclick = function() {
+  sidebar.classList.toggle("main__sidebar--mobile-hidden")
+
+  if (sidebar.classList.contains("main__sidebar--mobile-hidden")) {
+    toggle_button.innerHTML = "&#9776;";
+  } else {
+    toggle_button.innerHTML = "&#10006;"
+  }
+};
